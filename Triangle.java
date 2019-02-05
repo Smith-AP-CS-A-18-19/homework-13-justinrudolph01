8486b76e88db890f83955006ef6a2c5b6321cb5f
Rudolph <rudoljus@berkeleyprep.org>
2019-02-05 10:00:43 -0500

    finished

--- Triangle.java
+++ Triangle.java
@@ -1,4 +1,66 @@
 import java.awt.geom.Rectangle2D;
+public class Triangle implements SmithShape{
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private double angle;
+  private Rectangle2D rect;
+
+  public Triangle(int x, int y, int width, int height, double angle){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+  }
+
+  public int getX(){
+    return x;
+  }
+
+  public int getY(){
+    return y;
+  }
+
+  public int getWidth(){
+    return width;
+  }
+
+  public int getHeight(){
+    return height;
+  }
+
+  public double getArea(){
+    double area = (width * height / 2);
+    return area;
+  }
+
+  public double getAngle(){
+    return angle;
+  }
+
+  public Rectangle2D toSquare(){
+    rect = new Rectangle2D.Double(x, y, width, height);
+    return rect;
+  }
+
+  @Override
+  public String toString(){
+    String result = "Width: " + width;
+    result += "\n" + "Height: " + height + "\n" + "Angle: " + angle;
+    return result;
+  }
+
+}
+
+
+
+
+
+
+
+
 /* All SmithShape triangles have one edge that is on
  * the horizontal and are defined by the angle (in degrees)
  * of the lower left corner.
